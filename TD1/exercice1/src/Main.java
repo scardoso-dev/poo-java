d7af0edaa844577027ed5c5f2796ed3a78a2ed45
El Samouel <samuel.cardoso@ede.esiee-it.fr>
2023-01-20 10:01:14 +0100

    Modification du programme test de main.java

--- TD1/exercice1/src/Main.java
+++ TD1/exercice1/src/Main.java
@@ -3,7 +3,10 @@ import classes.Voiture;
 public class Main {
     public static void main(String[] args) {
         // Tableau contenant des voitures
-        Voiture[] voitures = {new Voiture("BMW",220.0f, 8), new Voiture("Ford",200.0f, 6), new Voiture("Audi",240.0f, 8)};
+        Voiture[] voitures = {new Voiture("DACIA",220.0f, 8), new Voiture("Mercede-Benz",200.0f, 6), new Voiture()};
+        voitures[2].setMarque("Audi");
+        voitures[2].setVitesse(240.0f);
+        voitures[2].setPuissance(8);
 
         // Parcours du tableau voitures à l'aide d'un foreach pour afficher chaque voiture
         for (Voiture voiture : voitures) {
